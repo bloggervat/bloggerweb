cd9bf308bf5314ad9c1d328dab801cb0684e1868
bloggervat <72654854+bloggervat@users.noreply.github.com>
2025-09-15 16:06:55 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-function createSnow() {
-    const snow = document.createElement('div');
-    snow.classList.add('snow');
-    
-    snow.style.left = Math.random() * 100 + 'vw';
-    snow.style.animationDuration = Math.random() * 3 + 2 + 's';
-    snow.style.opacity = Math.random();
-    snow.style.fontSize = Math.random() * 10 + 10 + 'px';
-    
-    snow.innerHTML = '❄';
-    
-    document.body.appendChild(snow);
-    
-    setTimeout(() => {
-        snow.remove();
-    }, 5000);
-}
 
-setInterval(createSnow, 100);
